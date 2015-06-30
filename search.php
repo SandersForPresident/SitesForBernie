7a576c5ba901f71b89714d8c9e3210fb9ec1d9ae
Atticus White <contact@atticuswhite.com>
2015-06-30 17:28:07 -0400

    search

---
+++ search.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<?php if (!have_posts()): ?>
+  Sorry, no results were found.
+  <?php get_search_form(); ?>
+<?php endif; ?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <h2><?php the_title(); ?></h2>
+  <?php the_content(); ?>
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
