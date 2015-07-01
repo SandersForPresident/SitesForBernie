33d4b2d2f7a03fdf3539bf358ed5d5b5653f00fa
Atticus White <contact@atticuswhite.com>
2015-07-01 10:33:37 -0400

    events custom post type

--- lib/init.php
+++ lib/init.php
@@ -17,3 +17,28 @@ function widgets_init() {
   // register any widget areas
 }
 add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
+
+/**
+ * Registers custom post types
+ */
+function register_custom_post_types () {
+  $postType = array(
+    'labels' => array (
+      'name' => 'Events',
+      'singular_name' => 'Event',
+      'edit_item' => 'Edit Event',
+      'view_item' => 'View Event',
+      'menu_name' => 'Events'
+    ),
+    'capability_type' => 'post',
+    'public' => true,
+    'show_ui' => true,
+    'has_archive' => true,
+    'hierarchical' => true,
+    'show_in_nav_menus' => true,
+    'rewrite' => array('slug' => 'events'),
+    'supports' => array('title', 'editor', 'revisions', 'thumbnail')
+  );
+  register_post_type('event', $postType);
+}
+add_action('init', __NAMESPACE__ . '\\register_custom_post_types');
