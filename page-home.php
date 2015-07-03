53d5eebfcd3340d36069a975de4a046490059a12
Atticus White <contact@atticuswhite.com>
2015-07-03 18:38:34 -0400

    current page context

--- page-home.php
+++ page-home.php
@@ -1,16 +1,21 @@
 <?php
 /* Template Name: Home */
 get_header();
+the_post();
 ?>
 
-<div class="container">
+<article class="container">
   <div class="page-container">
     <div class="page-title">
       <h2>Welcome To</h2>
       <h1><?php the_title(); ?></h1>
     </div>
 
+    <div class="rte">
+      <?php the_content(); ?>
+    </div>
+
   </div>
-</div>
+</article>
 
 <?php get_footer(); ?>
