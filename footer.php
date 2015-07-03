472050c4272fd218c480d721f953e8baa1af9688
Atticus White <contact@atticuswhite.com>
2015-07-03 15:30:17 -0400

    more styling and utils

--- footer.php
+++ footer.php
@@ -5,43 +5,46 @@
 
   <footer>
     <div class="container">
-      <div class="row">
+      <div class="offset">
 
-        <!-- Social Links -->
-        <div class="column">
-          <h2><?php echo $footer->leftNavigation->title; ?></h2>
-          <ul>
-            <?php foreach ($footer->leftNavigation->items as $item) : ?>
-              <li>
-                <a href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a>
-              </li>
-            <?php endforeach; ?>
-          </ul>
+        <div class="row">
+
+          <!-- Social Links -->
+          <div class="column">
+            <h2><?php echo $footer->leftNavigation->title; ?></h2>
+            <ul>
+              <?php foreach ($footer->leftNavigation->items as $item) : ?>
+                <li>
+                  <a href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a>
+                </li>
+              <?php endforeach; ?>
+            </ul>
+          </div>
+          <!-- end: Social Links -->
+
+          <!-- Presidential Campaign Links -->
+          <div class="column">
+            <h2>Bernie</h2>
+            <ul>
+              <li>Link</li>
+            </ul>
+          </div>
+          <!-- end: Presidental Campaign Links -->
+
+          <!-- Organization Links -->
+          <div class="column">
+            <h2><?php echo $footer->rightNavigation->title; ?></h2>
+            <ul>
+              <?php foreach ($footer->rightNavigation->items as $item) : ?>
+                <li>
+                  <a href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a>
+                </li>
+              <?php endforeach; ?>
+            </ul>
+          </div>
+          <!-- end: Oragnization Links -->
+
         </div>
-        <!-- end: Social Links -->
-
-        <!-- Presidential Campaign Links -->
-        <div class="column">
-          <h2>Bernie</h2>
-          <ul>
-            <li>Link</li>
-          </ul>
-        </div>
-        <!-- end: Presidental Campaign Links -->
-
-        <!-- Organization Links -->
-        <div class="column">
-          <h2><?php echo $footer->rightNavigation->title; ?></h2>
-          <ul>
-            <?php foreach ($footer->rightNavigation->items as $item) : ?>
-              <li>
-                <a href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a>
-              </li>
-            <?php endforeach; ?>
-          </ul>
-        </div>
-        <!-- end: Oragnization Links -->
-
       </div>
     </div>
   </footer>
