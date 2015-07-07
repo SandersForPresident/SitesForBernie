ae5af74d7d0310044ab4e4b5d624f6bb8a07b653
Atticus White <contact@atticuswhite.com>
2015-07-06 22:03:28 -0400

    :fire: the constants

--- lib/services/events/remote_events.php
+++ lib/services/events/remote_events.php
@@ -3,16 +3,6 @@ namespace SandersForPresident\Wordpress\Services\Events;
 
 class RemoteEventService {
   const FEED_ENDPOINT = 'https://secure.berniesanders.com/page/event/search_results';
-  const QUERY_PARAM_ORDERBY = 'orderby';
-  const QUERY_PARAM_ZIP_RADIUS = 'zip_radius';
-  const QUERY_PARAM_COUNTRY = 'country';
-  const QUERY_PARAM_COUNTRY_US = 'US';
-  const QUERY_PARAM_RADIUS_UNIT = 'radius_unit';
-  const QUERY_PARAM_RADIUS_UNIT_MI = 'mi';
-  const QUERY_PARAM_DATE = 'date';
-  const QUERY_PARAM_FORMAT = 'format';
-  const QUERY_PARAM_FORMAT_JSON = 'json';
-  const QUERY_PARAM_ZIP_MAX_RADIUS = 100;
 
   public function getAllEvents() {
     $params = $this->constructListParams();
@@ -32,24 +22,19 @@ class RemoteEventService {
   }
 
   private function constructSearchParams($zip) {
-    $params = array();
-    $params[QUERY_PARAM_ORDERBY] = QUERY_PARAM_DATE;
-    $params[QUERY_PARAM_ZIP_RADIUS] = array(
-      $zip,
-      QUERY_PARAM_ZIP_MAX_RADIUS
+    return array(
+      'orderby' => 'date',
+      'zip_radius' => array($zip, 100),
+      'country' => 'US',
+      'radius_unit' => 'mi',
+      'format' => 'json'
     );
-    $params[QUERY_PARAM_COUNTRY] = QUERY_PARAM_COUNTRY_US;
-    $params[QUERY_PARAM_RADIUS_UNIT] = QUERY_PARAM_RADIUS_UNIT_MI;
-    $params[QUERY_PARAM_FORMAT] = QUERY_PARAM_FORMAT_JSON;
   }
 
   private function constructListParams() {
-    $params = array();
-    $params[QUERY_PARAM_ORDERBY] = QUERY_PARAM_ZIP_RADIUS;
-    $params[QUERY_PARAM_FORMAT] = QUERY_PARAM_FORMAT_JSON;
+    return array (
+      'orderby' => 'date',
+      'format' => 'json'
+    );
   }
-}
-
-class RemoteEventServiceResponse {
-  private $data;
 }
