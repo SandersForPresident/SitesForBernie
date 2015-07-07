e65f3a7a6e39b4e20110ff4fe401b61fb48b90c8
Atticus White <contact@atticuswhite.com>
2015-07-07 01:29:02 -0400

    js build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
     jscs = require('gulp-jscs'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat');
 
 gulp.task('styles', function () {
   return gulp.src('assets/less/main.less')
   .pipe(less())
   .pipe(gulp.dest('assets/dist'));
+});
+
+gulp.task('js:client', function () {
+  gulp.src('assets/js/**/*.js')
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest('assets/dist'));
+})
+
+gulp.task('js:vendor', function () {
+  gulp.src([])
+  .pipe(concat('vendor.js'))
+  .pipe(gulp.dest('assets/dist'))
 });
 
 gulp.task('lint', function () {
@@ -16,8 +29,9 @@ gulp.task('lint', function () {
   .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('build', ['styles']);
+gulp.task('build', ['styles', 'js:client', 'js:vendor']);
 gulp.task('default', ['build']);
 gulp.task('watch', ['build'], function () {
   gulp.watch('assets/less/**/*.less', ['styles']);
+  gulp.watch('assets/js/**/*.js', ['js:client']);
 });
