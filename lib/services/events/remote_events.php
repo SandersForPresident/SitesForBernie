e56e47964957d7fd92690b5584c2ef376d16d46e
Atticus White <contact@atticuswhite.com>
2015-07-06 22:11:13 -0400

    remote event service

--- lib/services/events/remote_events.php
+++ lib/services/events/remote_events.php
@@ -3,17 +3,42 @@ namespace SandersForPresident\Wordpress\Services\Events;
 
 class RemoteEventService {
   const FEED_ENDPOINT = 'https://secure.berniesanders.com/page/event/search_results';
+  const FEED_CACHE_KEY = 'remote_events';
+  const FEED_CACHE_TTL = 900; // 15 minutes
 
+  /**
+   * Gets all events
+   */
   public function getAllEvents() {
-    $params = $this->constructListParams();
-    return $this->makeRequest();
+    $events = $this->loadCache();
+    if ($events) {
+      return $events;
+    } else {
+      $params = $this->constructListParams();
+      $events = $this->makeRequest();
+      $this->updateCache($events);
+      return $events;
+    }
   }
 
+  /**
+   * Searches events within an area
+   */
   public function searchEvents($zip) {
-    $params = $this->constructSearchParams($zip);
-    return $this->makeRequest($params);
+    $events = $this->loadCache($zip);
+    if ($events) {
+      return $events;
+    } else {
+      $params = $this->constructSearchParams($zip);
+      $events = $this->makeRequest($params);
+      $this->updateCache($events, $zip);
+      return $events;
+    }
   }
 
+  /**
+   * Performs the request for the events
+   */
   private function makeRequest($params = array()) {
     $requestUri = FEED_ENDPOINT;
     $requestUri .= http_build_query($params);
@@ -21,6 +46,23 @@ class RemoteEventService {
     return json_decode($response);
   }
 
+  /**
+   * Load the cached events, optionally with a grouping by zip
+   */
+  private function loadCache($group = null) {
+    return wp_cache_get(self::FEED_CACHE_KEY, $group);
+  }
+
+  /**
+   * Update the cache, optionally with a grouping by zip
+   */
+  private function updateCache($data, $group = null) {
+    return wp_cache_set(self::FEED_CACHE_KEY, $events, $group, self::FEED_CACHE_TTL);
+  }
+
+  /**
+   * Construct the search request params
+   */
   private function constructSearchParams($zip) {
     return array(
       'orderby' => 'date',
@@ -31,6 +73,9 @@ class RemoteEventService {
     );
   }
 
+  /**
+   * Construct the list request params
+   */
   private function constructListParams() {
     return array (
       'orderby' => 'date',
