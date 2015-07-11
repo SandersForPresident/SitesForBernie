db1e13177581cd5f8c7763c44377d62e97b27e63
Zack Cote <zackwcote@gmail.com>
2015-07-11 09:54:48 -0400

    so aggressive

--- lib/init.php
+++ lib/init.php
@@ -22,11 +22,11 @@ add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
  * Registers nav menus
  */
 function nav_menus_init() {
-  register_nav_menus(array (
+  register_nav_menus(array(
     'header' => 'Site Navigation',
     'footer_social' => 'Social Links',
     'footer_organize' => 'Organize Links'
-  ));
+    ));
 }
 add_action('init', __NAMESPACE__ . '\\nav_menus_init');
 
