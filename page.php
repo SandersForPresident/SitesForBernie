f73b804a9b2dd7853088eaa0e597b490d9f4d420
Anne Hawley Brett <annehawleybrett@gmail.com>
2015-07-16 19:26:41 -0400

    adds page-container class to sample page, fixing the lack of margins

--- page.php
+++ page.php
@@ -1,8 +1,9 @@
 <?php get_header(); ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <h2><?php the_title(); ?></h2>
-  <?php the_content(); ?>
-<?php endwhile; ?>
-
+<div class="page-container">
+  <?php while (have_posts()) : the_post(); ?>
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+  <?php endwhile; ?>
+</div>
 <?php get_footer(); ?>
