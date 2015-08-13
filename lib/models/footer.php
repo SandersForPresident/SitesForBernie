c2778c23509c3e1b85621d22b8a02954769c4e0d
Atticus White <contact@atticuswhite.com>
2015-08-12 20:25:24 -0400

    :fire: right navigation in the footer model

--- lib/models/footer.php
+++ lib/models/footer.php
@@ -4,23 +4,16 @@ namespace SandersForPresident\Wordpress\Models;
 
 class FooterModel extends BaseModel {
   const LEFT_NAVIGATION_SLUG = 'footer_social';
-  const RIGHT_NAVIGATION_SLUG = 'footer_organize';
 
   public $leftNavigation;
-  public $rightNavigation;
 
   public function __construct() {
     $this->navLocations = get_nav_menu_locations();
     $this->prepareLeftNavigation();
-    $this->prepareRightNavigation();
   }
 
   public function hasLeftNavigation() {
     return !empty($this->leftNavigation);
-  }
-
-  public function hasRightNavigation() {
-    return !empty($this->rightNavigation);
   }
 
   public function prepareLeftNavigation() {
@@ -30,18 +23,6 @@ class FooterModel extends BaseModel {
     $menu = wp_get_nav_menu_object($this->navLocations[self::LEFT_NAVIGATION_SLUG]);
     $menuItems = wp_get_nav_menu_items($menu->term_id);
     $this->fillModelAttributes($this->leftNavigation, array(
-      'title' => $menu->name,
-      'items' => $menuItems
-    ));
-  }
-
-  public function prepareRightNavigation() {
-    if (!array_key_exists(self::RIGHT_NAVIGATION_SLUG, $this->navLocations)) {
-      return;
-    }
-    $menu = wp_get_nav_menu_object($this->navLocations[self::RIGHT_NAVIGATION_SLUG]);
-    $menuItems = wp_get_nav_menu_items($menu->term_id);
-    $this->fillModelAttributes($this->rightNavigation, array(
       'title' => $menu->name,
       'items' => $menuItems
     ));
