155100eacc8d9a486c05d2db33d2dadeaccca3d4
Atticus White <contact@atticuswhite.com>
2015-08-12 23:09:01 -0400

    page title

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?>>
   <head>
+  	<title><?php wp_title( '|', true, 'right' ); ?></title>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
