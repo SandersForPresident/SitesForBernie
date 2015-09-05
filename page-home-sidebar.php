01c3913db4493c5f4c43cf404d7aa19c09d63522
Jason Braun <jasonbraun@me.com>
2015-09-05 14:32:34 -0700

    Add home sidebar widget area template
    Add page-home-sidebar.php template and widget model for users who want a sidebar home page with widgets.

---
+++ page-home-sidebar.php
@@ -0,0 +1,27 @@
+<?php
+/* Template Name: Home with Sidebar */
+get_header();
+the_post();
+?>
+
+<article class="container home">
+  <div class="page-container">
+    <div class="page-title">
+      <h3>Welcome To</h3>
+      <h2><?php the_title(); ?></h2>
+    </div>
+    <div class="row">
+      <div class="col-md-9">
+        <section>
+          <div class="rte">
+            <?php the_content(); ?>
+          </div>
+        </section>
+      </div>
+      <div class="col-md-3">
+        <?php if (dynamic_sidebar( 'page' ) ); ?>
+      </div>
+    </div>
+</article>
+
+<?php get_footer(); ?>
